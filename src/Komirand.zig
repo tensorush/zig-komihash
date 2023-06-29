0e9049618b08aa5d43d79e377a5baa2fdf3ce4db
Jora Troosh <tensorush@gmail.com>
2023-06-29 12:48:58 +0300

    Update public API

--- src/Komirand.zig
+++ src/Komirand.zig
@@ -6,7 +6,7 @@ const std = @import("std");
 const utils = @import("utils.zig");
 const tests = @import("tests.zig");
 
-const Komirand = @This();
+pub const Komirand = @This();
 
 /// Base seed.
 seed1: u64 = 0,
